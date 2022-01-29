00f1e8b85e6faf1a23fa9e3cfb2ef0888e7ba045
Iryna Ohurtsova <og.irkaa@gmail.com>
2022-01-29 22:38:38 +0200

    add payment controller

--- src/main/java/org/bus/ticket/payment/controller/PaymentController.java
+++ src/main/java/org/bus/ticket/payment/controller/PaymentController.java
@@ -2,6 +2,7 @@ package org.bus.ticket.payment.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.bus.ticket.payment.dto.PayTicketDto;
+import org.bus.ticket.payment.dto.PayTicketResultDto;
 import org.bus.ticket.payment.entity.Payment;
 import org.bus.ticket.payment.entity.PaymentStatus;
 import org.bus.ticket.payment.service.PaymentService;
@@ -17,22 +18,22 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping("/buy")
-    public long buyTicket(@RequestBody PayTicketDto payTicketDto) {
-        return paymentService.buyTicket(payTicketDto);
+    public PayTicketResultDto buyTicket(@RequestBody PayTicketDto payTicketDto) {
+        return new PayTicketResultDto(paymentService.buyTicket(payTicketDto));
     }
 
     @GetMapping("/{paymentId}/status")
-    public PaymentStatus getStatusById(@PathVariable long paymentId) {
+    public PaymentStatus getStatusById(@PathVariable("paymentId") long paymentId) {
         return paymentService.getStatusById(paymentId);
     }
 
     @GetMapping("/{status}")
-    public List<Payment> findAllByPaymentStatus(@PathVariable PaymentStatus paymentStatus) {
+    public List<Payment> findAllByPaymentStatus(@PathVariable("status") PaymentStatus paymentStatus) {
         return paymentService.findAllByPaymentStatus(paymentStatus);
     }
 
     @PostMapping("/update")
-    public void buyTicket(@RequestBody List<Payment> payments) {
+    public void saveAll(@RequestBody List<Payment> payments) {
         paymentService.saveAll(payments);
     }
 
---
+++ src/main/java/org/bus/ticket/payment/dto/PayTicketResultDto.java
@@ -0,0 +1,9 @@
+package org.bus.ticket.payment.dto;
+
+import lombok.Value;
+
+@Value
+public class PayTicketResultDto {
+
+    long paymentId;
+}
